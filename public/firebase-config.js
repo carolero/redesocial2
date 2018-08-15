471ebf07108e5e1f25fb550d79b7dd8df01127d4
Bruna <brujvieira@yahoo.com.br>
2018-08-15 15:54:01 -0300

    primeiro

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyA5nx7xmM183SKKr32Bt55n-68JDGtFe70",
+  authDomain: "redesocial-97f5f.firebaseapp.com",
+  databaseURL: "https://redesocial-97f5f.firebaseio.com",
+  projectId: "redesocial-97f5f",
+  storageBucket: "",
+  messagingSenderId: "890016274005"
+};
+firebase.initializeApp(config);
